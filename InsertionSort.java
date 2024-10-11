8d9e93650b217e0b3528fdfbd56f52f5fdd2cb90
hazlmae01 <hazlmae1122@gmail.com>
2024-10-11 15:21:44 +0800

    Created InsertionSort.java

---
+++ InsertionSort.java
@@ -0,0 +1,46 @@
+import java.util.Scanner;
+
+public class InsertionSort {
+    
+     //Operation for Insertion
+    public static void InsertionSort(int[] array) {
+        for (int i = 1; i < array.length; i++) {
+            int key = array[i];
+            int j = i - 1;
+            while (j >= 0 && array[j] > key) {
+                array[j + 1] = array[j];
+                j = j - 1;
+            }
+            array[j + 1] = key;
+        }
+    }
+    
+    
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        // Input the number of elements
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+
+        int[] array = new int[n];
+
+        // Input the elements of the array
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+
+        // Perform Insertion Sort
+        InsertionSort(array);
+
+        // Output the sorted array
+        System.out.println("Sorted array: ");
+        for (int i = 0; i < n; i++) {
+            System.out.print(array[i] + " ");
+        }
+        System.out.println();
+        
+        sc.close();
+    }
+}
