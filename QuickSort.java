39f492f908693c5f61766814226e8acf73388fd4
hazlmae01 <hazlmae1122@gmail.com>
2024-10-11 15:27:00 +0800

    Created QuickSort.java

---
+++ QuickSort.java
@@ -0,0 +1,44 @@
+import java.util.Scanner;
+
+public class QuickSort {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        
+        //Ask input from user
+        System.out.print("Enter the number of elements: ");
+        int n = sc.nextInt();
+        
+        //Declaration of an array
+        int[] array = new int[n];
+        System.out.println("Enter the elements of the array: ");
+        for (int i = 0; i < n; i++) {
+            array[i] = sc.nextInt();
+        }
+    }
+
+    //Operation for QuickSort
+    public static void quickSort(int[] array, int low, int high) {
+        if (low < high) {
+            int pi = partition(array, low, high);
+            quickSort(array, low, pi - 1);
+            quickSort(array, pi + 1, high);
+        }
+    }
+
+    public static int partition(int[] array, int low, int high) {
+        int pivot = array[high];
+        int i = low - 1;
+        for (int j = low; j < high; j++) {
+            if (array[j] < pivot) {
+                i++;
+                int temp = array[i];
+                array[i] = array[j];
+                array[j] = temp;
+            }
+        }
+        int temp = array[i + 1];
+        array[i + 1] = array[high];
+        array[high] = temp;
+        return i + 1;
+    }
+}
